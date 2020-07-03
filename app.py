8bae3ecf7733fd2e9595740b78bfe8624c5897c3
Osenias Oliveira <oseniasjunior@gmail.com>
2020-07-02 21:46:31 -0400

    Ajustes projeto crud com sqlite

--- app.py
+++ app.py
@@ -14,3 +14,46 @@ def django_initialize():
 
 if __name__ == '__main__':
     django_initialize()
+
+    from core import models
+    from datetime import date
+
+    # person = models.Person()
+    # person.name = 'Ozzy'
+    # person.gender = 'M'
+    # person.salary = 2000.00
+    # person.date_birth = date(1987, 10, 28)
+    # person.save()
+    #
+    # person.name = 'Ozzy Oliveira'
+    # person.save()
+    #
+    # person2 = models.Person.objects.create(
+    #     name='Gustavo',
+    #     gender='M',
+    #     salary=3000.00,
+    #     date_birth=date(2000, 1, 1)
+    # )
+    #
+    # data = {
+    #     'name': 'Taty',
+    #     'gender': 'F',
+    #     'salary': 4000.00,
+    #     'date_birth': date(2000, 1, 1)
+    # }
+    #
+    # models.Person.objects.create(**data)
+
+    person_list = models.Person.objects.all()
+
+    print(
+        person_list
+    )
+
+    person = models.Person.objects.get(id=1)
+    person2 = models.Person.objects.filter(id=2).first()
+    person3 = models.Person.objects.filter(name='Taty').first()
+
+    print(person.name)
+    print(person2.name)
+    print(person3.name)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class Person(models.Model):
+    id = models.AutoField(
+        db_column='id',
+        null=False,
+        primary_key=True
+    )
+    name = models.CharField(
+        db_column='tx_name',
+        null=False,
+        max_length=100
+    )
+    gender = models.CharField(
+        db_column='cs_gender',
+        null=False,
+        max_length=1
+    )
+    salary = models.DecimalField(
+        db_column='nb_salary',
+        null=False,
+        max_digits=10,
+        decimal_places=2
+    )
+    date_birth = models.DateField(
+        db_column='dt_birth',
+        null=False
+    )
+
+    class Meta:
+        db_table = 'person'
+        managed = True
